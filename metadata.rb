71bc748220cf87bbf59966823583e2fe1f6c12fa
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2022-06-22 02:10:14 -0400

    remove unused information

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Jimbo Dragon'
 maintainer_email 'jimbo_dragon@hotmail.com'
 license          'Apache 2.0'
 description      'Installs virtualbox'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.0.0'
 chef_version '>= 16.6.14'
 
