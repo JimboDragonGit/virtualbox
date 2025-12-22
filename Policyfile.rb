99cb5bf354661586de271bd9f4bf6e37e8bfed85
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2025-12-22 10:16:00 -0500

    Install webportal by default

--- Policyfile.rb
+++ Policyfile.rb
@@ -9,11 +9,12 @@ name 'virtualbox'
 # Where to find external cookbooks:
 default_source :supermarket
 
-# run_list: chef-client will run these recipes in the order specified.
-run_list 'virtualbox::default'
-
 # Specify a custom source for a single cookbook:
 cookbook 'virtualbox', path: '.'
 
+# run_list: chef-client will run these recipes in the order specified.
+run_list 'virtualbox::webportal'
+
+named_run_list :default, 'virtualbox::default'
 named_run_list :service, 'virtualbox::systemservice'
 named_run_list :webportal, 'virtualbox::webportal'
